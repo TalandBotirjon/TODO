b200fbb5c8e4cc8ef949c4aa371f1cf1a9d21c21
TalandBotirjon <ergashev_botirjon@mail.ru>
2022-11-24 20:15:33 +0500

    Todo models turn

--- todos/models.py
+++ todos/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+import  datetime
 # Create your models here.
 
 
@@ -7,6 +7,10 @@ class Todo(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
     tekshirish = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    do_success = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
+
+
